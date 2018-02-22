bdaf272ac585c6ccf58bb691047934cdcec6c07f
Anthony Neto <anthony.neto@gmail.com>
2018-02-22 11:10:22 -0800

    adding libregeek steams-tools repo

--- cookbooks/default/recipes/default.rb
+++ cookbooks/default/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+%w(
+  libregeek-archive-keyring.deb
+  steamos-tools-repo.deb
+).each do |pkg|
+  remote_file "#{Chef::Config[:file_cache_path]}/#{pkg}" do
+    source "http://packages.libregeek.org/#{pkg}"
+  end
+  dpkg_package "#{Chef::Config[:file_cache_path]}/#{pkg}"
+end
