ab5307351a10d46225a69c903af4d80cff1606bb
Anthony Neto <anthony.neto@gmail.com>
2018-02-22 11:54:01 -0800

    hopefully this is correct?

--- cookbooks/default/recipes/default.rb
+++ cookbooks/default/recipes/default.rb
@@ -4,19 +4,15 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-%w(
-  libregeek-archive-keyring.deb
-  steamos-tools-repo.deb
-).each do |pkg|
-  remote_file "#{Chef::Config[:file_cache_path]}/#{pkg}" do
-    source "http://packages.libregeek.org/#{pkg}"
-  end
-  dpkg_package "#{Chef::Config[:file_cache_path]}/#{pkg}"
+remote_file "#{Chef::Config[:file_cache_path]}/libregeek-archive-keyring.deb" do
+  source 'http://packages.libregeek.org/libregeek-archive-keyring.deb'
 end
+dpkg_package "#{Chef::Config[:file_cache_path]}/libregeek-archive-keyring.deb"
 
-apt_update 'update' do
-  action :nothing
-  subscribes :install, "dpkg_package[#{Chef::Config[:file_cache_path]}/steamos-tools-repo.deb]", :immediately
+apt_repository 'steamos-tools' do
+  uri 'http://packages.libregeek.org/steamos-tools/'
+  distribution 'brewmaster'
+  components %w(main games)
 end
 
 package 'retroarch'
